363f976227af1c96d9899b2eeff2dc82d70ca6f9
Ting Xu(徐婷) <xuting@shmpc23126.local>
2024-02-03 23:44:24 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-createApp(App).mount('#app')
+//引入element-plus插件和样式
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+//获取应用实例对象
+const app = createApp(App)
+//安装element-plus插件
+app.use(ElementPlus)
+//将应用挂载到挂载点上
+App.mount('#app')
